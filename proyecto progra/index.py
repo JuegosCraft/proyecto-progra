e8bd8ba1bf93f63a5493c9870bd43742d227a542
JuegosCraft <116029578+JuegosCraft@users.noreply.github.com>
2022-10-24 19:59:40 +0200

    actualizacion6

--- proyecto progra/index.py
+++ proyecto progra/index.py
@@ -1,115 +1,152 @@
 from tkinter import *
-from xml.dom.minidom import Identified
-import mysql.connector
+import mysql.connector 
+from tkinter import ttk
 from tkinter import messagebox
-#-----------------------------ventana menu dimenciones-----------------------------------
-e=Tk()
-e.title("Registro")
-e.geometry("700x300")
-e.config(bg="#B4C7E7")
-e.resizable(0,0)
-#-------------------------------base-----------------------------------
+
+v =Tk()
+v.title("Registro")
+v.geometry("500x400")
+v.config(bg="#D3FFC3")
+v.resizable(0,0)
+
 conexion=mysql.connector.connect(host="localhost",port="3306",user="root",password="")
 bd=conexion.cursor()
-bd.execute("CREATE DATABASE IF NOT EXISTS proyecto")
+bd.execute("CREATE DATABASE IF NOT EXISTS base_de_datos")
 bd.close()
-conexion.close()
-conexion=mysql.connector.connect(host="localhost",port="3306",user="root",password="",database="proyecto")
+
+conexion=mysql.connector.connect(host="localhost",port="3306",user="root",password="",database="base_de_datos")
 bd=conexion.cursor()
 bd.execute("CREATE TABLE IF NOT EXISTS login(usuario VARCHAR(25),contraseña VARCHAR(55))")
 bd.execute("CREATE TABLE IF NOT EXISTS registro(identificacion VARCHAR(25),nombre_y_apellidos VARCHAR(55),puesto_de_trabajo VARCHAR(55),usuario VARCHAR(55),contraseña VARCHAR(55))")
 bd.close()
-#----------------------ventana registro-------------------------------------
+
 def registro():
-#-----------------fuciones---------------------------    
     def agregar_usuario():
-            cur=conexion.cursor()
-            ide=txt_identificacion.get()
-            nom=txt_nombre.get()
-            cur.execute("update registro set nombre_y_apellidos=%s, puesto_de_trabajo=%s, usuario=%s, contraseña=%s where identificacion=%s",modificar_usuario)
-            conexion.commit()
-            cur.close()
-            messagebox.showinfo(message="los datos se modificaron",title="informacion")     
-            limpiar()
-        
-    def modificar_usuario():
-          cur = conexion.cursor()
-          modificar_usuario = txt_identificacion.get(), txt_nombre.get(), txt_trabajo.get(), txt_usuario.get(), txt_contraseña.get()
-          cur.execute("update registro set nombre_y_apellidos=%s, puesto_de_trabajo=%s, usuario=%s, contraseña=%s where identificacion=%s",modificar_usuario)
-          conexion.commit()
-          cur.close()
-          messagebox.showinfo(message="los datos se modificaron",title="informacion")     
-          limpiar()
-    def eliminar_usuario():
-             cur=conexion.cursor()
-             cod=txt_usuario.get()
-             cur.execute("delete from inventario where codigo ='{}'".format(cod))
-             conexion.commit()
-             cur.close()
-             messagebox.showinfo(message="se elimino el libro exitosamente",title="informacion")
-             txt_usuario.delete(0,END)
-             txt_contraseña.delete(0,END)
-    u= Tk()
-    u.title("Registro")
-    u.geometry("850x680")
-    u.config(bg="#B4C7E7")
-    u.resizable(0,0)
-#-----------------------------etiqueta registro-------------------------------------------------------------
-    etiqueta=Label(u,font=("arial",18,"bold"),text="identificacion:",bg="#B4C7E7").place(x=25,y=100)
-    etiqueta=Label(u,font=("arial",18,"bold"),text="nombre y apellido:",bg="#B4C7E7").place(x=25,y=200)
-    etiqueta=Label(u,font=("arial",18,"bold"),text="puesto de trabajo:",bg="#B4C7E7").place(x=25,y=300)
-    etiqueta=Label(u,font=("arial",18,"bold"),text="usuario:",bg="#B4C7E7").place(x=25,y=400)
-    etiqueta=Label(u,font=("arial",18,"bold"),text="contraseña:",bg="#B4C7E7").place(x=25,y=500)
-#------------------------txt registro-----------------------------------------------------------------------------------------------
-    txt_identificacion=Entry(u,font=("arial",18,"bold"),bg="#D0CECE").place(x=260,y=100)
-    txt_nombre=Entry(u,font=("arial",18,"bold"),bg="#D0CECE").place(x=260,y=200)
-    txt_trabajo=Entry(u,font=("arial",18,"bold"),bg="#D0CECE").place(x=260,y=300)
-    txt_usuario=Entry(u,font=("arial",18,"bold"),bg="#D0CECE").place(x=260,y=400)
-    txt_contraseña=Entry(u,font=("arial",18,"bold"),bg="#D0CECE").place(x=260,y=500)
-#-------------------------------botones registro-------------------------------------------------
-    boton_u1=Button(u,font=("arial",12,"bold"),text="consultar",width=20,height=2,bg="#0077AC",).place(x=600,y=100,)
-    boton_u2=Button(u,font=("arial",12,"bold"),text="agregar usuario",width=20,height=2,bg="#0077AC",command=agregar_usuario).place(x=50,y=600)
-    boton_u3=Button(u,font=("arial",12,"bold"),text="modificar usuario",width=20,height=2,bg="#0077AC",command=modificar_usuario).place(x=300,y=600)
-    boton_u4=Button(u,font=("arial",12,"bold"),text="eliminar usuario",width=20,height=2,bg="#0077AC",command=eliminar_usuario).place(x=550,y=600)
-#--------------------------------ventana login----------------------------------
-def login():
-#------------------------------------funciones de login-------------------------------------------------------
-    def agregar_libro():
         cur=conexion.cursor()
+        ide=txt_identificacion.get()
+        nom=txt_nombre.get()
+        tra=txt_trabajo.get()
         usu=txt_usuario.get()
         con=txt_contraseña.get()
-        cur.execute("insert into inventario(usuario,contraseña)values('{}','{}')".format(usuario,contraseña))
-        conexion.commit()#actualizar el envio de datos
+        cur.execute("insert into registro(identificacion,nombre_y_apellidos,puesto_de_trabajo,usuario,contraseña) values('{}','{}','{}','{}','{}')".format(ide,nom,tra,usu,con))
+        conexion.commit()
         cur.close()
-        messagebox.showinfo(message="login exitosos",title="informacion")
+        messagebox.showinfo(message="El nuevo usuario se guardo de forma exitosa", title="informacion al guardar")
+        txt_identificacion.delete(0,END)
+        txt_nombre.delete(0,END)
+        txt_trabajo.delete(0,END)
         txt_usuario.delete(0,END)
         txt_contraseña.delete(0,END)
+        txt_identificacion.focus()
 
-    def cancelar_login():
+    def buscar_usuario():
+        cur=conexion.cursor()
+        cur.execute("select * from registro")
+        datos=cur.fetchall()
+        txt_nombre.delete(0,END)
+        txt_trabajo.delete(0,END)
         txt_usuario.delete(0,END)
         txt_contraseña.delete(0,END)
-        messagebox.showinfo(message="se canselo el login",title="informacion")
-#-------------------------------------------------------------------------     
-    j = Tk()
-    j.title('Registro')
-    j.geometry('800x300')
-    j.resizable(0,0)
-    j.config(bg='#B4C7E7')
-#-------------------etiquesta login-------------------------------------------------------------------------
-    etiquetas_usuario = Label (j,font=('century',18,'bold'),text='Usuario',bg='#B4C7E7',width=20,height=1,bd=5,fg="#000000").place(x=10,y=45)
-    etiquetas_contraseña = Label (j,font=('century',18,'bold'),text='Contraseña',bg='#B4C7E7',width=20,height=1,bd=5,fg="#000000").place(x=1,y=150)
-#----------------------------txt login-------------------------------------------------------------------------------------------
-    txt_usuario = Entry(j,font=('century',18,'bold'),width=20,bg="#D0CECE").place(x=240,y=50)
-    txt_contraseña = Entry(j,font=('century',18,'bold'),width=20,bg="#D0CECE").place(x=250,y=150)
-#----------------------------------------------------------------------
-    boton_inventario = Button(j,font=("arial",12,"bold"),width=20,height=2,bg="#0077AC",text="ingresar sistema",).place(x=530,y=45)
-    boton_inventario = Button(j,font=("arial",12,"bold"),width=20,height=2,bg="#0077AC",text="cancelar",).place(x=540,y=145)
-#---------------------------ventana menu--------------------------------------------------
-boton_re = Button(e,font=("arial",12,"bold"),text="Resgistro",command=registro,width=20,height=2,bg="#0077AC").place(x=70,y=130)
-boton_log = Button(e,font=("arial",12,"bold"),text="Login",width=20,command=login,height=2,bg="#0077AC",).place(x=300,y=130)
+        for columna in datos:
+            if columna[0]==txt_identificacion.get():
+                txt_nombre.insert(0,columna[1])
+                txt_trabajo.insert(0,columna[2])
+                txt_usuario.insert(0,columna[3])
+                txt_contraseña.insert(0,columna[4])
+        txt_identificacion.focus()
+        cur.close()
+    
+    def limpiar():
+        txt_identificacion.delete(0,END)
+        txt_nombre.delete(0,END)
+        txt_trabajo.delete(0,END)
+        txt_usuario.delete(0,END)
+        txt_contraseña.delete(0,END)
+        txt_identificacion.focus()
 
-#------------------------etiqueta menu-----------------------------
-etiqueta = Label (e,font=('century',18,'bold'),text='Base de Datos',bg='#B4C7E7',width=20,height=1,bd=5,fg="#000000").place(x=120,y=30)
+    def eliminar_usuario():
+        cur=conexion.cursor()
+        ide=txt_identificacion.get()
+        cur.execute("delete from registro where identificacion='{}'".format(ide))
+        conexion.commit()
+        limpiar()
+        cur.close()
+        messagebox.showinfo(message="El usuario se borro con exito", title="informacion al eliminar")
 
-#--------------------------------------------------------------------
-e=mainloop()
+    def modificar_usuario():
+        cur=conexion.cursor()
+        modificar_datos=txt_nombre.get(),txt_trabajo.get(),txt_usuario.get(),txt_contraseña.get(),txt_identificacion.get()
+        cur.execute("update registro set nombre_y_apellidos=%s, puesto_de_trabajo=%s, usuario=%s, contraseña=%s where identificacion=%s",modificar_datos)
+        conexion.commit()
+        cur.close()
+        messagebox.showinfo(message="El usuario se modifico con exito", title="informacion al actualizar")
+        limpiar()
+    
+    inv = Tk()
+    inv.title("usuario")
+    inv.geometry("600x400")
+    inv.config(bg="#D0B7FF")
+    inv.resizable(0,0)
+    etiqueta=Label(inv,font=("arial",18,"bold"),text="registro",bg="#D0B7FF",).place(x=125,y=5)
+    etiqueta=Label(inv,font=("arial",18,"bold"),text="identificacion:",bg="#D0B7FF",).place(x=15,y=50)
+    txt_identificacion=Entry(inv,font=("arial",18,"bold"),bg="#FFFFFF")
+    txt_identificacion.place(x=125,y=50)
+    txt_identificacion.bind("<Return>",(lambda event:buscar_usuario()))
+    etiqueta=Label(inv,font=("arial",18,"bold"),text="nombre",bg="#D0B7FF",).place(x=15,y=100)
+    txt_nombre=Entry(inv,font=("arial",18,"bold"),bg="#FFFFFF")
+    txt_nombre.place(x=125,y=100)
+    etiqueuta=Label(inv,font=("arial",18,"bold"),text="trabajo",bg="#D0B7FF",).place(x=15,y=150)
+    txt_trabajo=Entry(inv,font=("arial",18,"bold"),bg="#FFFFFF")
+    txt_trabajo.place(x=125,y=150)
+    etiqueta=Label(inv,font=("arial",18,"bold"),text="usuario",bg="#D0B7FF",).place(x=15,y=200)
+    txt_usuario=Entry(inv,font=("arial",18,"bold"),bg="#FFFFFF",width=13)
+    txt_usuario.place(x=217,y=200)
+    etiqueta=Label(inv,font=("arial",18,"bold"),text="contraseña",bg="#D0B7FF",).place(x=15,y=250)
+    txt_contraseña=Entry(inv,font=("arial",18,"bold"),bg="#FFFFFF")
+    txt_contraseña.place(x=125,y=250)
+    boton_inv2=Button(inv,font=("arial",10,"bold"),text="agregar usuario",width=15,bg="#FFFFFF",command=agregar_usuario).place(x=200,y=300)
+    boton_inv3=Button(inv,font=("arial",10,"bold"),text="modificar usuario",width=15,bg="#FFFFFF",command=modificar_usuario).place(x=50,y=300)
+    boton_inv4=Button(inv,font=("arial",10,"bold"),text="eliminar usuario",width=15,bg="#FFFFFF",command=eliminar_usuario).place(x=350,y=300)
+    boton_inv5=Button(inv,font=("arial",10,"bold"),text="buscar",width=15,bg="#FFFFFF",command=buscar_usuario).place(x=400,y=50)
+    boton_inv6=Button(inv,font=("arial",10,"bold"),text="cancelar",width=15,bg="#FFFFFF",command=limpiar).place(x=400,y=350)
+    
+def login():
+    def buscar_usuario():
+        cur=conexion.cursor()
+        cur.execute("select * from registro")
+        datos=cur.fetchall()
+        for columna in datos:
+            if txt_usuario2 == columna[4]:
+                if txt_contraseña2 == columna[5]:
+                    messagebox.showinfo(message="El usuario existe", title="informacion del usuario")
+            else:
+                messagebox.showinfo(message="El usuario no existe", title="informacion del usuario")
+        cur.close()
+    
+    pre = Tk()
+    pre.title("login")
+    pre.geometry("600x500")
+    pre.config(bg="#FFFFC7")
+    pre.resizable(0,0)
+    etiqueta=Label(pre,font=("arial",18,"bold"),text="login",bg="#FFFFC7",).place(x=200,y=5)
+    etiqueta=Label(pre,font=("arial",18,"bold"),text="usuario",bg="#FFFFC7",).place(x=15,y=50)
+    txt_usuario2=Entry(pre,font=("arial",18,"bold"),bg="#FFFFFF",width=15,).place(x=200,y=50)
+    etiqueta=Label(pre,font=("arial",18,"bold"),text="contraseña",bg="#FFFFC7",).place(x=15,y=100)
+    txt_contraseña2=Entry(pre,font=("arial",18,"bold"),bg="#FFFFFF",width=15).place(x=200,y=100)
+    boton_pre2=Button(pre,font=("arial",10,"bold"),text="buscar",command=buscar_usuario,width=15,bg="#FFFFFF").place(x=450,y=125)
+    boton_pre3=Button(pre,font=("arial",10,"bold"),text="cancelar",width=15,bg="#FFFFFF").place(x=450,y=350)
+    
+
+
+etiqueta=Label(v,font=("century",24,"bold"),text="Biblioteca virtual",bg="#D3FFC3").place(x=125,y=25)
+
+boton_inv=Button(v,font=("century",12,"bold"),text="registro", command=registro,width=15,height=5,bg="#FFFFFF").place(x=50,y=100)
+
+boton_cli=Button(v,font=("century",12,"bold"),text="login", command=login,width=15,height=5,bg="#FFFFFF").place(x=300,y=100)
+
+
+
+
+v.mainloop()
+
+
